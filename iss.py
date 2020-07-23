24136dade364d2488f77e6c998007a181150c451
Keith Garcia <Keith.Garcia444@gmail.com>
2020-07-23 15:23:46 -0400

    final with fixes

--- iss.py
+++ iss.py
@@ -7,7 +7,7 @@ import turtle
 import time
 
 
-def get_houstan_astros():
+def get_houston_astros():
     res = requests.get('http://api.open-notify.org/astros.json')
     return res.json()
 
@@ -29,23 +29,26 @@ def init_turtle():
     rise_time = time.ctime(next_pass["response"][0]["risetime"])
 
     display = turtle.Screen()
-    display.setup(width=720, height=360, startx=0, starty=0)
+    display.setup(width=800, height=400, startx=0, starty=0)
     display.setworldcoordinates(-180, -90, 180, 90)
     display.bgpic('map.gif')
     display.register_shape('iss.gif')
 
+    iss.shape("iss.gif")
+    iss.pu()
     iss.goto(-86.1581, 39.7684)
     iss.dot(4, "yellow")
     iss.color('blue')
-    iss.shape("iss.gif")
-    iss.penup()
-    iss.write(rise_time, align='right', font=("Courier", 22))
-
+    iss.write(rise_time, align='right', font=("Courier", 14))
     return iss
 
 
 def main():
-    data = get_houstan_astros()
+    data = get_houston_astros()
+    coords = get_coords()
+    eastern_time = time.ctime()
+    print("The {} is currently located at {}: Latitude and {}: Longitude as of {}.".format(
+        data['people'][0]['craft'], coords['iss_position']['latitude'], coords['iss_position']['longitude'], eastern_time))
     print("There are {} astronauts on the {} as of today.".format(data['number'],
                                                                   data['people'][0]['craft']))
     for astro in data["people"]:
@@ -60,6 +63,7 @@ def main():
         if heading > 0.0:
             iss.setheading(heading)
         iss.goto(lon, lat)
+        time.sleep(16)
 
 
 if __name__ == '__main__':
